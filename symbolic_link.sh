7b322b887c7c2c56360362e65f037365da856adc
bububuroro <boboarasute99@gmail.com>
2018-01-28 23:24:43 +0900

    add .xremp.conf

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current=$(cd $(dirname $0) && pwd)
-files=('.vimrc' '.zprofile' '.zshenv' '.zshrc' '.tmux.conf')
+files=('.vimrc' '.zprofile' '.zshenv' '.zshrc' '.tmux.conf' '.xremap.conf')
 
 for file in ${files[@]}
 do
